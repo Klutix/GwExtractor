051f54c652b5743f9b8b059f2915af5052344664
Gabriel Wohlford <GabrielWohlford@gmail.com>
2019-03-24 04:33:16 -0600

    Added Project Directory

---
+++ GWExtractor/Backup/GWExtractor/Form1.vb
@@ -0,0 +1,271 @@
+﻿Imports SHDocVw
+Imports NDde.Client
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.IO
+Imports Microsoft.Office.Interop
+
+Public Class GWXtractor
+    Dim mstrURL As String
+    Dim mstrSourceCode As String
+    Dim mExcelApp As Excel.Application
+    Dim mExcelWB As Excel.Workbook
+    Dim mExcelWS As Excel.Worksheet
+
+    Dim mstrFilePath As String
+
+
+
+
+    '=========================FIREFOX=================================
+    'For firefox ; Download the library from http://ndde.codeplex.com/ 
+    Public Shared Function GetFirefoxUrl() As String
+        Dim dde As New DdeClient("Firefox", "WWW_GetWindowInfo")
+        dde.Connect()
+        Dim url() As String = Split(dde.Request("URL", Integer.MaxValue), ",")
+        dde.Disconnect()
+        Return url(0).Replace("""", "")
+    End Function
+
+    Private Sub BtnGrab_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnGrab.Click
+        txtUrl.Text = (GetFirefoxUrl())
+        RichTextBox1.Text = getPageSource(txtUrl.Text)
+        mstrSourceCode = RichTextBox1.Text
+        Parse_Source()
+        
+
+    End Sub
+
+    Function getPageSource(ByVal url As String) As String
+
+        Dim sourcecode As String = ""
+
+        Try
+
+            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(url)
+            Dim response As System.Net.HttpWebResponse = request.GetResponse()
+            Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+
+            sourcecode = sr.ReadToEnd()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return sourcecode
+
+    End Function
+    ' for 'Internet explorer'
+    Public Function getAllRunningIEURLs()
+        Try
+            Dim list As New ListBox
+            For Each browser As InternetExplorer In New ShellWindows()
+                txtUrl.Text = browser.LocationURL.ToString
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Function
+
+    Private Sub GWXtractor_Disposed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Disposed
+        mExcelApp.Quit()
+
+        mExcelApp = Nothing
+        mExcelWB = Nothing
+        mExcelWS = Nothing
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function Parse_Source()
+        Dim strTitle As String = ""
+        Dim strSKU As String = ""
+        Dim strUPC As String = ""
+        Dim strDescription As String = ""
+        Dim strMPN As String = ""
+        Dim strPrice As String = ""
+        Dim strMPrice As String = ""
+
+        Dim strLine As String = ""
+
+        Dim lngcntBail As Long = 0
+
+        Dim strRemainingString As String
+
+
+        Try
+            Using sr As StringReader = New StringReader(mstrSourceCode)
+                Do Until lngcntBail = 10
+                    strLine = sr.ReadLine
+
+                    'check if we have reached the end
+                    If (strLine = "") Then
+                        lngcntBail += 1
+                    Else
+                        lngcntBail = 0
+                    End If
+
+                    If (strLine Is Nothing = False) Then
+
+                        'grab Title
+                        If (strLine.Contains("og:title")) Then
+
+                            'Grabs the begging of all relavent into variable so for a cleaner starting point
+                            strRemainingString = strLine.Substring(InStr(strLine, "og:title") + 18)
+
+                            'Remove the irrelavent data
+                            strTitle = strRemainingString.Substring(0, strRemainingString.IndexOf(""""))
+                            txtTitle.Text = strTitle
+                        End If
+
+                        'grab Description
+
+                        'grab SKU
+
+                        'grab UPC
+                        If (strLine.Contains("gtin13")) Then
+                            'Grabs the begging of all relavent into variable so for a cleaner starting point
+                            strRemainingString = strLine.Substring(InStr(strLine, "gtin13") + 7)
+
+                            'Remove the irrelavent data
+                            strUPC = strRemainingString.Substring(0, strRemainingString.IndexOf("<"))
+                            txtUPC.Text = strUPC
+                        End If
+
+                        'grab MPN 
+                        If (strLine.Contains("""mpn""")) Then
+                            'Grabs the begging of all relavent into variable so for a cleaner starting point
+                            strRemainingString = strLine.Substring(InStr(strLine, """mpn""") + 5)
+
+                            'Remove the irrelavent data
+                            strMPN = strRemainingString.Substring(0, strRemainingString.IndexOf("<"))
+                            txtMPN.Text = strMPN
+                        End If
+
+
+
+                        'grab Price
+                        If (strLine.Contains("""prcIsum""")) Then
+                            'Grabs the begging of all relavent into variable so for a cleaner starting point
+                            strRemainingString = strLine.Substring(InStr(strLine, ">US") + 3)
+
+                            'Remove the irrelavent data
+                            strPrice = strRemainingString.Substring(0, strRemainingString.IndexOf("<"))
+                            txtPrice.Text = strPrice
+                        End If
+
+                        'grap Mprice
+                    End If
+
+                Loop
+                Write_Excel(strTitle, strPrice, strDescription, strUPC, strSKU, strMPN, strMPrice, txtUrl.Text)
+
+
+                mExcelApp = Nothing
+                mExcelWB = Nothing
+                mExcelWS = Nothing
+            End Using
+
+        Catch ex As Exception
+            MsgBox(ex.Message & vbNewLine & ex.StackTrace)
+        End Try
+
+
+
+    End Function
+
+    Private Function Write_Excel(ByVal strTitle As String, ByVal strPrice As String, Optional ByVal strDescription As String = "", Optional ByVal strUPC As String = "", Optional ByVal strSKU As String = "", Optional ByVal strMPN As String = "", Optional ByVal strMPrice As String = "", Optional ByVal strURL As String = "") As Boolean
+        Try
+            Dim lngRow As Long
+
+
+            If (mExcelApp Is Nothing) Then
+                Create_ExcelFile(txtSelectedFile.Text)
+            End If
+
+            lngRow = Return_RowCnt()
+            mExcelWS.Cells(lngRow, 1) = strTitle
+            mExcelWS.Cells(lngRow, 2) = strDescription
+            mExcelWS.Cells(lngRow, 3) = "&" & strUPC
+            mExcelWS.Cells(lngRow, 4) = strSKU
+            mExcelWS.Cells(lngRow, 5) = strMPN
+            mExcelWS.Cells(lngRow, 6) = strPrice
+            mExcelWS.Cells(lngRow, 7) = strMPrice
+            mExcelWS.Cells(lngRow, 8) = strURL
+
+            mExcelWB.SaveAs(txtSelectedFile.Text)
+            mExcelApp.Quit()
+
+            mExcelApp = Nothing
+            mExcelWB = Nothing
+            mExcelWS = Nothing
+
+        Catch ex As Exception
+            MsgBox(ex.Message & vbNewLine & ex.StackTrace)
+        End Try
+
+
+    End Function
+
+    'Creates Initial Excel File if none has been selected
+    Private Function Create_ExcelFile(Optional ByRef strFullPathName = "")
+
+        Try
+            Dim lngRow As Long = 1
+
+            mExcelApp = New Excel.Application
+
+            If (mExcelApp Is Nothing) Then
+                MsgBox("Excel not Property Installed")
+                Return False
+                Exit Function
+            End If
+
+            mExcelWB = mExcelApp.Workbooks.Add
+            mExcelWS = mExcelWB.Worksheets.Add
+
+            mExcelWS.Cells(lngRow, 1) = "Title"
+            mExcelWS.Cells(lngRow, 2) = "Description"
+            mExcelWS.Cells(lngRow, 3) = "UPC"
+            mExcelWS.Cells(lngRow, 4) = "SKU"
+            mExcelWS.Cells(lngRow, 5) = "MPN"
+            mExcelWS.Cells(lngRow, 6) = "Price"
+            mExcelWS.Cells(lngRow, 7) = "MPrice"
+            mExcelWS.Cells(lngRow, 8) = "URL"
+
+            If (strFullPathName = "") Then strFullPathName = CurDir() & "\ProductInfo.xls"
+
+            txtSelectedFile.Text = strFullPathName
+
+        Catch ex As Exception
+
+        End Try
+
+
+    End Function
+
+    Private Sub LblCurrentFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LblCurrentFile.Click
+        FolderBrowserDialog1.ShowDialog()
+        'FolderBrowserDialog1.SelectedPath = CurDir() & "\exceldump.xls"
+        mstrFilePath = FolderBrowserDialog1.SelectedPath
+        txtSelectedFile.Text = mstrFilePath & "\ProductInfo.xls"
+    End Sub
+
+    Private Function Return_RowCnt()
+        Dim lngrow As Long = 1
+
+        Do Until String.IsNullOrEmpty(mExcelWS.Cells(lngrow, 1).value)
+            lngrow += 1
+        Loop
+
+        Return lngrow
+
+    End Function
+End Class
+
+
+
---
+++ GWExtractor/GWExtractor/Form1.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GWXtractor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnGrab = New System.Windows.Forms.Button
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.txtUrl = New System.Windows.Forms.TextBox
+        Me.lblSaorceCode = New System.Windows.Forms.Label
+        Me.txtDescription = New System.Windows.Forms.TextBox
+        Me.lblDescription = New System.Windows.Forms.Label
+        Me.txtTitle = New System.Windows.Forms.TextBox
+        Me.lblTitle = New System.Windows.Forms.Label
+        Me.txtSKU = New System.Windows.Forms.TextBox
+        Me.lblSKU = New System.Windows.Forms.Label
+        Me.txtUPC = New System.Windows.Forms.TextBox
+        Me.txtMPN = New System.Windows.Forms.TextBox
+        Me.txtPrice = New System.Windows.Forms.TextBox
+        Me.txtMapPrice = New System.Windows.Forms.TextBox
+        Me.lblUrl = New System.Windows.Forms.Label
+        Me.lblMPN = New System.Windows.Forms.Label
+        Me.lblUPC = New System.Windows.Forms.Label
+        Me.lblPrice = New System.Windows.Forms.Label
+        Me.lblMapPrice = New System.Windows.Forms.Label
+        Me.grpWebSource = New System.Windows.Forms.GroupBox
+        Me.chkEbay = New System.Windows.Forms.CheckBox
+        Me.chk = New System.Windows.Forms.CheckBox
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog
+        Me.txtSelectedFile = New System.Windows.Forms.TextBox
+        Me.LblCurrentFile = New System.Windows.Forms.Label
+        Me.grpWebSource.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BtnGrab
+        '
+        Me.BtnGrab.Location = New System.Drawing.Point(422, 38)
+        Me.BtnGrab.Name = "BtnGrab"
+        Me.BtnGrab.Size = New System.Drawing.Size(92, 26)
+        Me.BtnGrab.TabIndex = 0
+        Me.BtnGrab.Text = "Go"
+        Me.BtnGrab.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(24, 73)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(490, 146)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'txtUrl
+        '
+        Me.txtUrl.Location = New System.Drawing.Point(47, 12)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(467, 20)
+        Me.txtUrl.TabIndex = 2
+        '
+        'lblSaorceCode
+        '
+        Me.lblSaorceCode.AutoSize = True
+        Me.lblSaorceCode.Location = New System.Drawing.Point(21, 54)
+        Me.lblSaorceCode.Name = "lblSaorceCode"
+        Me.lblSaorceCode.Size = New System.Drawing.Size(69, 13)
+        Me.lblSaorceCode.TabIndex = 4
+        Me.lblSaorceCode.Text = "Sauce Code:"
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescription.Location = New System.Drawing.Point(24, 280)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(490, 20)
+        Me.txtDescription.TabIndex = 5
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescription.Location = New System.Drawing.Point(21, 264)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(60, 13)
+        Me.lblDescription.TabIndex = 6
+        Me.lblDescription.Text = "Description"
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTitle.Location = New System.Drawing.Point(24, 244)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(490, 20)
+        Me.txtTitle.TabIndex = 7
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(21, 228)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(27, 13)
+        Me.lblTitle.TabIndex = 8
+        Me.lblTitle.Text = "Title"
+        '
+        'txtSKU
+        '
+        Me.txtSKU.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSKU.Location = New System.Drawing.Point(24, 327)
+        Me.txtSKU.Name = "txtSKU"
+        Me.txtSKU.Size = New System.Drawing.Size(181, 20)
+        Me.txtSKU.TabIndex = 9
+        '
+        'lblSKU
+        '
+        Me.lblSKU.AutoSize = True
+        Me.lblSKU.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSKU.Location = New System.Drawing.Point(23, 389)
+        Me.lblSKU.Name = "lblSKU"
+        Me.lblSKU.Size = New System.Drawing.Size(29, 13)
+        Me.lblSKU.TabIndex = 10
+        Me.lblSKU.Text = "SKU"
+        '
+        'txtUPC
+        '
+        Me.txtUPC.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUPC.Location = New System.Drawing.Point(26, 405)
+        Me.txtUPC.Name = "txtUPC"
+        Me.txtUPC.Size = New System.Drawing.Size(179, 20)
+        Me.txtUPC.TabIndex = 11
+        '
+        'txtMPN
+        '
+        Me.txtMPN.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMPN.Location = New System.Drawing.Point(24, 366)
+        Me.txtMPN.Name = "txtMPN"
+        Me.txtMPN.Size = New System.Drawing.Size(181, 20)
+        Me.txtMPN.TabIndex = 12
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrice.Location = New System.Drawing.Point(281, 327)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(77, 20)
+        Me.txtPrice.TabIndex = 13
+        '
+        'txtMapPrice
+        '
+        Me.txtMapPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMapPrice.Location = New System.Drawing.Point(281, 356)
+        Me.txtMapPrice.Name = "txtMapPrice"
+        Me.txtMapPrice.Size = New System.Drawing.Size(77, 20)
+        Me.txtMapPrice.TabIndex = 14
+        '
+        'lblUrl
+        '
+        Me.lblUrl.AutoSize = True
+        Me.lblUrl.Location = New System.Drawing.Point(12, 15)
+        Me.lblUrl.Name = "lblUrl"
+        Me.lblUrl.Size = New System.Drawing.Size(32, 13)
+        Me.lblUrl.TabIndex = 15
+        Me.lblUrl.Text = "URL:"
+        '
+        'lblMPN
+        '
+        Me.lblMPN.AutoSize = True
+        Me.lblMPN.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMPN.Location = New System.Drawing.Point(21, 311)
+        Me.lblMPN.Name = "lblMPN"
+        Me.lblMPN.Size = New System.Drawing.Size(31, 13)
+        Me.lblMPN.TabIndex = 16
+        Me.lblMPN.Text = "MPN"
+        '
+        'lblUPC
+        '
+        Me.lblUPC.AutoSize = True
+        Me.lblUPC.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUPC.Location = New System.Drawing.Point(23, 350)
+        Me.lblUPC.Name = "lblUPC"
+        Me.lblUPC.Size = New System.Drawing.Size(29, 13)
+        Me.lblUPC.TabIndex = 17
+        Me.lblUPC.Text = "UPC"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrice.Location = New System.Drawing.Point(244, 330)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(31, 13)
+        Me.lblPrice.TabIndex = 18
+        Me.lblPrice.Text = "Price"
+        '
+        'lblMapPrice
+        '
+        Me.lblMapPrice.AutoSize = True
+        Me.lblMapPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMapPrice.Location = New System.Drawing.Point(235, 359)
+        Me.lblMapPrice.Name = "lblMapPrice"
+        Me.lblMapPrice.Size = New System.Drawing.Size(40, 13)
+        Me.lblMapPrice.TabIndex = 19
+        Me.lblMapPrice.Text = "MPrice"
+        '
+        'grpWebSource
+        '
+        Me.grpWebSource.Controls.Add(Me.chk)
+        Me.grpWebSource.Controls.Add(Me.chkEbay)
+        Me.grpWebSource.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpWebSource.Location = New System.Drawing.Point(390, 319)
+        Me.grpWebSource.Name = "grpWebSource"
+        Me.grpWebSource.Size = New System.Drawing.Size(103, 77)
+        Me.grpWebSource.TabIndex = 20
+        Me.grpWebSource.TabStop = False
+        Me.grpWebSource.Text = "Source"
+        '
+        'chkEbay
+        '
+        Me.chkEbay.AutoSize = True
+        Me.chkEbay.Location = New System.Drawing.Point(11, 19)
+        Me.chkEbay.Name = "chkEbay"
+        Me.chkEbay.Size = New System.Drawing.Size(50, 17)
+        Me.chkEbay.TabIndex = 0
+        Me.chkEbay.Text = "Ebay"
+        Me.chkEbay.UseVisualStyleBackColor = True
+        '
+        'chk
+        '
+        Me.chk.AutoSize = True
+        Me.chk.Location = New System.Drawing.Point(10, 50)
+        Me.chk.Name = "chk"
+        Me.chk.Size = New System.Drawing.Size(64, 17)
+        Me.chk.TabIndex = 1
+        Me.chk.Text = "Amazon"
+        Me.chk.UseVisualStyleBackColor = True
+        '
+        'txtSelectedFile
+        '
+        Me.txtSelectedFile.Location = New System.Drawing.Point(64, 431)
+        Me.txtSelectedFile.Name = "txtSelectedFile"
+        Me.txtSelectedFile.Size = New System.Drawing.Size(429, 20)
+        Me.txtSelectedFile.TabIndex = 21
+        '
+        'LblCurrentFile
+        '
+        Me.LblCurrentFile.AutoSize = True
+        Me.LblCurrentFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblCurrentFile.ForeColor = System.Drawing.Color.Red
+        Me.LblCurrentFile.Location = New System.Drawing.Point(21, 434)
+        Me.LblCurrentFile.Name = "LblCurrentFile"
+        Me.LblCurrentFile.Size = New System.Drawing.Size(45, 13)
+        Me.LblCurrentFile.TabIndex = 22
+        Me.LblCurrentFile.Text = "Output"
+        '
+        'GWXtractor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(526, 465)
+        Me.Controls.Add(Me.LblCurrentFile)
+        Me.Controls.Add(Me.txtSelectedFile)
+        Me.Controls.Add(Me.grpWebSource)
+        Me.Controls.Add(Me.lblMapPrice)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblUPC)
+        Me.Controls.Add(Me.lblMPN)
+        Me.Controls.Add(Me.lblUrl)
+        Me.Controls.Add(Me.txtMapPrice)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtMPN)
+        Me.Controls.Add(Me.txtUPC)
+        Me.Controls.Add(Me.lblSKU)
+        Me.Controls.Add(Me.txtSKU)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.lblSaorceCode)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.BtnGrab)
+        Me.Name = "GWXtractor"
+        Me.Text = "Xtractor"
+        Me.grpWebSource.ResumeLayout(False)
+        Me.grpWebSource.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnGrab As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtUrl As System.Windows.Forms.TextBox
+    Friend WithEvents lblSaorceCode As System.Windows.Forms.Label
+    Friend WithEvents txtDescription As System.Windows.Forms.TextBox
+    Friend WithEvents lblDescription As System.Windows.Forms.Label
+    Friend WithEvents txtTitle As System.Windows.Forms.TextBox
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents txtSKU As System.Windows.Forms.TextBox
+    Friend WithEvents lblSKU As System.Windows.Forms.Label
+    Friend WithEvents txtUPC As System.Windows.Forms.TextBox
+    Friend WithEvents txtMPN As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrice As System.Windows.Forms.TextBox
+    Friend WithEvents txtMapPrice As System.Windows.Forms.TextBox
+    Friend WithEvents lblUrl As System.Windows.Forms.Label
+    Friend WithEvents lblMPN As System.Windows.Forms.Label
+    Friend WithEvents lblUPC As System.Windows.Forms.Label
+    Friend WithEvents lblPrice As System.Windows.Forms.Label
+    Friend WithEvents lblMapPrice As System.Windows.Forms.Label
+    Friend WithEvents grpWebSource As System.Windows.Forms.GroupBox
+    Friend WithEvents chk As System.Windows.Forms.CheckBox
+    Friend WithEvents chkEbay As System.Windows.Forms.CheckBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents txtSelectedFile As System.Windows.Forms.TextBox
+    Friend WithEvents LblCurrentFile As System.Windows.Forms.Label
+
+End Class
